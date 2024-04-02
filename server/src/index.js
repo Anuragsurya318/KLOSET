708753c86e1354467102bab4bdde131c462bcb67
Anuragsurya318 <suryawanshianurag28@gmail.com>
2024-04-02 19:35:51 +0530

    all functionality done now only styling remained

---
+++ server/src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/user.js";
+import { productRouter } from "./routes/product.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/user", userRouter);
+app.use("/product", productRouter);
+
+mongoose.connect(
+  "mongodb+srv://anuragsurya100:3huC6haEccpmJsOD@cluster0.epazexe.mongodb.net/project2"
+);
+
+app.listen(3000, () => {
+  console.log("SERVER STARTED");
+});
